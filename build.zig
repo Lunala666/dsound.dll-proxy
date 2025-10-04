67e95bc41c2da779c506d1ba798f5052dcc8e051
I.M. Luna <115100370+Lunala666@users.noreply.github.com>
2025-10-04 17:11:32 +0100

    Add files via upload

---
+++ build.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addSharedLibrary(.{
+        .name = "proxy",
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(lib);
+
+    lib.linkLibC();
+}
